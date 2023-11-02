ac720ef139345e492d32b20f48e1c7e38ee82350
jackroberts1993 <jackroberts1993@gmail.com>
2023-11-02 16:33:59 +0000

    Changed post artefact check to demo.war

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+	@RequestMapping("/")
+	public String hello1(){
+		return "Hello";
+	}
+
+	@RequestMapping("/hello")
+	public String hello2(){
+		return "Hello again";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
